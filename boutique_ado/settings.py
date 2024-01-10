95c31acbda76bb67f25290396765c2cc5017192a
Eric Blake <blake.eric@gmail.com>
2024-01-10 19:25:06 +0000

    Update settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = False
 
-ALLOWED_HOSTS = ['boutique-ado-eb-2ecdad62b1ef.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['boutique-ado-eb-2ecdad62b1ef.herokuapp.com', 'localhost']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN' 
 
