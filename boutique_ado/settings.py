bf335e3467d76605f8d09db7db2aaf46a0f8dbee
Eric Blake <blake.eric@gmail.com>
2024-01-11 11:47:24 +0000

    Update settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-eb-2ecdad62b1ef.herokuapp.com', 'localhost','8000-ericblake-boutiqueadov1-nfbqlyots3o.ws-eu107.gitpod.io']
 
@@ -131,17 +131,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-
     DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-     }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-    }
+
 
     
 
